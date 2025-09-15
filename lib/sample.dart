2071fc61c72f653ffb561210fc0df4420b2b39c4
Elijah Mar Rosialda <elijahejrosialda@gmail.com>
2025-09-15 12:46:35 -0700

    Add files via upload

--- lib/sample.dart
+++ lib/sample.dart
@@ -1,109 +1,155 @@
 import 'package:flutter/material.dart';
 
-class AddBottomAppBar extends StatefulWidget {
-  const AddBottomAppBar({super.key});
+class AddBottomAppBar2 extends StatefulWidget {
+  const AddBottomAppBar2({super.key});
 
   @override
-  State<AddBottomAppBar> createState() => _AddBottomAppBarState();
+  State<AddBottomAppBar2> createState() => _AddBottomAppBar2State();
 }
 
-class _AddBottomAppBarState extends State<AddBottomAppBar> {
-  int _currentIndex = 0;
+class _AddBottomAppBar2State extends State<AddBottomAppBar2> {
+  int _selectedIndex = 0;
 
-  final Map<String, Widget> _pagesPair = {
-    "School": const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          Icons.school_rounded,
-          size: 50,
+  late final Map<String, Widget> _pagesPair = {
+    "Home": Center(
+      child: Text(
+        "Home Screen",
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
         ),
-        Text(
-          "School Screen",
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-        ),
-      ],
+      ),
     ),
-    "Schedule": const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          Icons.schedule_rounded,
-          size: 50,
-        ),
-        Text(
-          "Schedule Screen",
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-        ),
-      ],
-    ),
-    "Score": const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          Icons.score_rounded,
-          size: 50,
-        ),
-        Text(
-          "Score Screen",
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-        ),
-      ],
-    ),
+    "Container": _buildContainer(),
+    "Column": _buildColumn(),
+    "Row": _buildRow(),
   };
 
-  _buildBottomItems(IconData icon, String label) {
-    return BottomNavigationBarItem(icon: Icon(icon), label: label);
+  _buildBottomItems(IconData icon) {
+    return BottomNavigationBarItem(
+      icon: Icon(icon),
+      label: _pagesPair.keys.elementAt(_selectedIndex),
+    );
+  }
+
+  Widget _buildContainer() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          "Container Widget",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+        SizedBox(height: 30),
+        Container(width: 100, height: 100, color: Colors.blue),
+      ],
+    );
+  }
+
+  Widget _buildColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          "Column Widget",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+        SizedBox(height: 30),
+        Column(
+          children: [
+            Container(width: 100, height: 100, color: Colors.blue),
+            SizedBox(height: 10),
+            Container(width: 100, height: 100, color: Colors.blue),
+            SizedBox(height: 10),
+            Container(width: 100, height: 100, color: Colors.blue),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildRow() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          "Column Widget",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+        SizedBox(height: 30),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(width: 100, height: 100, color: Colors.blue),
+            SizedBox(width: 50),
+            Container(width: 100, height: 100, color: Colors.blue),
+            SizedBox(width: 50),
+            Container(width: 100, height: 100, color: Colors.blue),
+          ],
+        ),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_pagesPair.keys.elementAt(_currentIndex)),
-        titleTextStyle:
-            const TextStyle(fontWeight: FontWeight.normal, fontSize: 24),
-        leading: Builder(builder: (context) {
-          return IconButton(
+        title: Text(_pagesPair.keys.elementAt(_selectedIndex)),
+        titleTextStyle: const TextStyle(
+          fontWeight: FontWeight.normal,
+          fontSize: 24,
+        ),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
               onPressed: () => Scaffold.of(context).openDrawer(),
-              icon: const Icon(Icons.menu));
-        }),
+              icon: const Icon(Icons.menu, color: Colors.blueAccent),
+            );
+          },
+        ),
         actions: <Widget>[
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.notifications_rounded,
-                color: Colors.amber,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.person_rounded,
-                color: Colors.blue,
-              )),
-          IconButton(
-              onPressed: () {}, icon: const Icon(Icons.settings_rounded)),
+          const Icon(Icons.settings_rounded),
+          const Icon(Icons.notifications_rounded),
+          const Icon(Icons.person_rounded),
         ],
       ),
-      drawer: const Drawer(
-        width: 200,
-      ),
+      drawer: Drawer(),
       body: SafeArea(
-        child: Center(child: _pagesPair.values.elementAt(_currentIndex)),
+        child: Center(child: _pagesPair.values.elementAt(_selectedIndex)),
       ),
       bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: Colors.blue,
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          items: [
-            _buildBottomItems(Icons.school_rounded, "School"),
-            _buildBottomItems(Icons.schedule_rounded, "Schedule"),
-            _buildBottomItems(Icons.score_rounded, "Score")
-          ]),
+        unselectedItemColor: Colors.black,
+        enableFeedback: true,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.blue.shade800,
+        unselectedIconTheme: IconThemeData(color: Colors.grey),
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          _buildBottomItems(Icons.home),
+          _buildBottomItems(Icons.apps),
+          _buildBottomItems(Icons.view_column_sharp),
+          _buildBottomItems(Icons.table_rows),
+        ],
+      ),
     );
   }
 }
