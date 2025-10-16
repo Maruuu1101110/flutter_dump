7821049a53d503a6893f2e651daf7922b322e075
Elijah Mar Rosialda <elijahejrosialda@gmail.com>
2025-10-16 02:47:17 -0700

    Add files via upload

---
+++ lib/login_form.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final TextEditingController _unameController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
+  bool _showPass = false;
+
+  Widget _textInput(bool obs, TextEditingController controller, String hintText,
+      double textFormWidth, icon) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+      width: textFormWidth,
+      height: 40,
+      decoration: BoxDecoration(
+          border: Border.all(width: 2),
+          borderRadius: BorderRadius.circular(12)),
+      child: TextFormField(
+        obscureText: obs,
+        decoration: InputDecoration(
+            border: InputBorder.none, hintText: hintText, suffixIcon: icon),
+        controller: controller,
+      ),
+    );
+  }
+
+  Widget _verticalLayout(textFormWidth) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+            width: 500,
+            height: 300,
+            child: Image.asset('assets/login_image.png')),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _textInput(false, _unameController, 'Username', textFormWidth,
+                Icon(Icons.person)),
+            const SizedBox(
+              height: 10,
+            ),
+            _textInput(
+              _showPass,
+              _passController,
+              "Password",
+              textFormWidth,
+              IconButton(
+                  onPressed: () => setState(() {
+                        _showPass = !_showPass;
+                      }),
+                  icon: Icon(Icons.remove_red_eye)),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+                width: textFormWidth,
+                child: ElevatedButton(
+                    onPressed: () {}, child: const Text("Sign In")))
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _horizontalLayout(textFormWidth) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+            width: 700,
+            height: 700,
+            child: Image.asset('assets/login_image.png')),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _textInput(false, _unameController, 'Username', textFormWidth,
+                Icon(Icons.person)),
+            const SizedBox(
+              height: 10,
+            ),
+            _textInput(
+                _showPass,
+                _passController,
+                "Password",
+                textFormWidth,
+                IconButton(
+                    onPressed: () => setState(() {
+                          _showPass = !_showPass;
+                        }),
+                    icon: Icon(Icons.remove_red_eye))),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+                width: textFormWidth,
+                child: ElevatedButton(
+                    onPressed: () {}, child: const Text("Sign In")))
+          ],
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    late final double textFormWidth = MediaQuery.of(context).size.width * 0.5;
+    late final generalWidth = MediaQuery.of(context).size.width;
+    print(generalWidth);
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+            child: generalWidth <= 1155
+                ? _verticalLayout(textFormWidth)
+                : _horizontalLayout(textFormWidth.clamp(0, 500))));
+  }
+}
